9ff92b508d7d6b9ff08dc3b2bfe1edcb48c7aad1
DmelladoH <mellado.m2@gmail.com>
2023-05-05 22:06:50 +0200

    add isLoading state to codeContext

--- big-o-calculator/src/context/CodeContext.tsx
+++ big-o-calculator/src/context/CodeContext.tsx
@@ -2,16 +2,26 @@ import { createContext, useState } from 'react'
 
 interface CodeContextValue {
   code: string
+  isLoading: boolean
   setCode: (code: string) => void
+  setIsLoading: (isLoading: boolean) => void
 }
 
-export const CodeContext = createContext<CodeContextValue>({ code: '', setCode: () => {} })
+const EMPTY_CONTEXT = {
+  code: '',
+  isLoading: false,
+  setCode: () => {},
+  setIsLoading: () => {}
+}
+
+export const CodeContext = createContext<CodeContextValue>(EMPTY_CONTEXT)
 
 export function CodeProvider ({ children }: { children: React.ReactNode }) {
   const [code, setCode] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   return (
-   <CodeContext.Provider value={{ code, setCode }}>
+   <CodeContext.Provider value={{ code, setCode, isLoading, setIsLoading }}>
       {children}
     </CodeContext.Provider>
   )
