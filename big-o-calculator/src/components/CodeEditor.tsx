e95be0920d2cc51b1f66c58d8a391ab078a5af97
DmelladoH <mellado.m2@gmail.com>
2023-05-05 16:04:04 +0200

    create Editor component

--- big-o-calculator/src/components/CodeEditor.tsx
+++ big-o-calculator/src/components/CodeEditor.tsx
@@ -1,22 +1,47 @@
 import { useRef } from 'react'
-import Editor from '@monaco-editor/react'
+import { Editor } from '@monaco-editor/react'
 import type * as monaco from 'monaco-editor'
+import { useCode } from '../hooks/useCode'
 
 export default function CodeEditor () {
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null)
 
+  const { setCode } = useCode()
+
   function handleEditorDidMount (editor: any, monaco: any) {
     editorRef.current = editor
+
+    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+      noSemanticValidation: true,
+      noSyntaxValidation: true
+    })
+
+    editor.onDidChangeModelContent(() => {
+      const value = editor.getValue()
+      setCode(value)
+    })
+  }
+
+  const options: monaco.editor.IEditorConstructionOptions = {
+    wordWrap: 'on',
+    minimap: { enabled: false },
+    folding: false,
+    lineNumbersMinChars: 3,
+    fontSize: 16,
+    scrollBeyondLastLine: false,
+    automaticLayout: true,
+    contextmenu: false
   }
 
   return (
-    <section className='h-96 flex align-middle justify-center m-9'>
+    <section className='align-middle justify-center sm:h-full'>
         <Editor
-            height="100%"
-            defaultLanguage="javascript"
-            theme="vs-dark"
-            onMount={handleEditorDidMount}
-        />
+          language='typescript'
+          theme='vs-dark'
+          onMount={handleEditorDidMount}
+          options={options}
+          />;
+
     </section>
   )
 }
