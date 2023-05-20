85bc268f8a964b596e3117acc5ba0490ed59f190
DmelladoH <mellado.m2@gmail.com>
2023-05-20 15:31:09 +0200

    fix Routing

--- big-o-calculator/src/App.tsx
+++ big-o-calculator/src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import './App.css'
-import { Route, Router } from 'wouter'
+import { Route, Switch } from 'wouter'
 import Home from './pages/Home'
 import AboutBigO from './pages/AboutBigO'
 import Header from './components/Header'
@@ -9,10 +9,10 @@ function App () {
   return (
     <div className="flex flex-col align-middle w-screen bg-background">
       <Header />
-      <Router>
+      <Switch>
         <Route path='/' component={Home} />
         <Route path='/big-o' component={AboutBigO} />
-      </Router>
+      </Switch>
 
     </div>
   )
